72ca56e36480dce20c8bb740a2f903da52c4530e
realimrandev <imransabir2005@gmail.com>
2026-04-01 09:03:26 +0500

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+export default function Header(){
+    return(
+        <>
+          <div class="wrapper">
+    <div class="title"><span>Login Form</span></div>
+
+    <form action="#">
+      <div class="row">
+        <i class="fas fa-user"></i>
+        <input type="text" placeholder="Email or Phone" required />
+      </div>
+
+      <div class="row">
+        <i class="fas fa-lock"></i>
+        <input type="password" placeholder="Password" required />
+      </div>
+
+      <div class="pass"><a href="#">Forgot password?</a></div>
+
+      <div class="row button">
+        <input type="submit" value="Login" />
+      </div>
+
+      <div class="signup-link">Not a member? <a href="#">Signup now</a></div>
+    </form>
+  </div>
+        </>
+    );
+}
